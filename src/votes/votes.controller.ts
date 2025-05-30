cc11de86d2891b695630271cb5d0597146bd4520
Isaac Wilson <iswilson.dev@gmail.com>
2025-05-30 11:28:52 +0000

    refactor: refactor ranking

--- src/votes/votes.controller.ts
+++ src/votes/votes.controller.ts
@@ -6,6 +6,8 @@ import {
   UseGuards,
   Get,
   Param,
+  Delete,
+  BadRequestException,
 } from '@nestjs/common';
 import { VotesService } from './votes.service';
 import { JwtAuthGuard } from '../users/guards/jwt.guard';
@@ -14,6 +16,8 @@ import { Request } from 'express';
 import { Equal } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { UserDto } from 'src/users/dto/user-dto';
+import { K } from 'handlebars';
+import { privateDecrypt } from 'crypto';
 
 @Controller('votes')
 @UseGuards(JwtAuthGuard)
@@ -22,7 +26,7 @@ export class VotesController {
 
   @Post(':recipientId')
   async vote(@Req() req, @Param('recipientId') recipientId: number) {
-     console.log('req.user.id', req.user.id);
+    console.log('req.user.id', req.user.id);
     return new VoteDto(
       await this.votesService.castVote(req.user.id, recipientId),
       `${req.protocol}://${req.get('host')}`,
@@ -76,11 +80,84 @@ export class VotesController {
     });
   }
 
-  @Get('ranking/overall-best/:gender')
-  async overallBestGender(
+  @Get('ranking/overall-best')
+  async overallBestGender(@Req() req: Request) {
+    const overallMale = await this.getOverallRanking(req, 'male');
+    const overallFemale = await this.getOverallRanking(req, 'female');
+    return {
+      male: overallMale.at(-1),
+      female: overallFemale.at(-1),
+    };
+  }
+
+  @Get('ranking/best/:subsidiary')
+  async bestBySubsidiaryAndGender(
     @Req() req: Request,
-    @Param('gender') gender: 'female' | 'male',
+    @Param('subsidiary') subsidiary: Subsidiary,
   ) {
+    if (
+      !['estate-masters', 'hannex', 'nestas', 'dwellys'].includes(subsidiary)
+    ) {
+      throw new BadRequestException(
+        `Invalid subsidiary,must be one of the following: ${['estate-masters', 'hannex', 'nestas', 'dwellys'].join(', ')}`,
+      );
+    }
+
+    const male = await this.getBestSubsidiary(req, subsidiary, 'male');
+    const female = await this.getBestSubsidiary(req, subsidiary, 'female');
+
+    return {
+      male,
+      female,
+    };
+  }
+
+  @Delete(':id')
+  async deleteVote(@Param('id') id: number, @Req() req: Request) {
+    return this.votesService.deleteVote(id);
+  }
+  private async getBestSubsidiary(
+    req: Request,
+    subsidiary: Subsidiary,
+    gender: Gender,
+  ) {
+    const votes = await this.votesService.findByAny({
+      where: {
+        recipient: {
+          subsidiary: Equal(subsidiary),
+          gender: Equal(gender),
+        },
+      },
+    });
+
+    const users = votes.map((vote) => vote.recipient);
+    const uniqueUsers = Array.from(new Set(users.map((user) => user.id))).map(
+      (id) => users.find((user) => user.id === id),
+    );
+    const userVotes = uniqueUsers.map((user) => {
+      const userVotesCount = votes.filter(
+        (vote) => vote.recipient.id === user?.id,
+      ).length;
+
+      return {
+        user,
+        totalVotes: userVotesCount,
+      };
+    });
+    userVotes.sort((a, b) => a.totalVotes - b.totalVotes);
+
+    return {
+      user: userVotes.at(-1)?.user
+        ? new UserDto(
+            userVotes.at(-1)?.user as User,
+            `${req.protocol}://${req.get('host')}`,
+          )
+        : null,
+      totalVotes: userVotes.at(-1)?.totalVotes || 0,
+    };
+  }
+
+  private async getOverallRanking(req: Request, gender: Gender) {
     const votes = await this.votesService.findByAny({
       where: {
         recipient: {
@@ -105,7 +182,7 @@ export class VotesController {
       };
     });
 
-    userVotes.sort((a, b) => a.totalVotes - b.totalVotes );
+    userVotes.sort((a, b) => a.totalVotes - b.totalVotes);
 
     return userVotes.map((uv) => {
       return {
@@ -117,48 +194,7 @@ export class VotesController {
       };
     });
   }
-
-
-   @Get('ranking/best/:subsidiary/:gender')
-   async bestBySubsidiaryAndGender(
-      @Req() req: Request,
-      @Param('subsidiary') subsidiary: 'estate-masters' | 'hannex' | 'nestas' | 'dwellys',
-      @Param('gender') gender: 'male' | 'female')
-   {
-      const votes = await this.votesService.findByAny({
-         where: {
-            recipient: {
-               subsidiary: Equal(subsidiary),
-               gender: Equal(gender)
-            }
-         }
-      });
-
-
-      const users = votes.map((vote) => vote.recipient);
-      const uniqueUsers = Array.from(new Set(users.map((user) => user.id))).map(
-         (id) => users.find((user) => user.id === id),
-      );
-      const userVotes = uniqueUsers.map((user) => {
-         const userVotesCount = votes.filter(
-            (vote) => vote.recipient.id === user?.id,
-         ).length;
-
-         return {
-            user,
-            totalVotes: userVotesCount,
-         };
-      }
-      );
-      userVotes.sort((a, b) => a.totalVotes - b.totalVotes);
-
-      return {
-         user: userVotes.at(-1)?.user ? new UserDto(
-            userVotes.at(-1)?.user as User,
-            `${req.protocol}://${req.get('host')}`,
-         ) : null,
-         totalVotes: userVotes.at(-1)?.totalVotes || 0,
-      }
-
-   }
 }
+
+export type Subsidiary = 'estate-masters' | 'hannex' | 'nestas' | 'dwellys';
+export type Gender = 'male' | 'female';
